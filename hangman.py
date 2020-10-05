6ca293c666ec8201b79a449a83d765221a424dfa
MichaelHagler <juggleforlife@gmail.com>
2020-10-05 06:48:58 -0700

    Update hangman.py

--- hangman.py
+++ hangman.py
@@ -2,23 +2,25 @@
 import random
 
 words = ["pie", "programmer", "python"]
-quit = False
 hidden_word = random.choice(list(words))
-guess_counter = 0
+guess_counter = 6
 
 #shows how many letters are in the word
 print("_ "*(len(hidden_word)))
 print(f"""The hidden word is {len(hidden_word)} letters""")
 
 
-while guess_counter < 6:
+while guess_counter > 0:
     guess = input("Guess a letter. ")
 
     # check if letter is in hidden_word
     for char in hidden_word:
-        if guess != char in hidden_word:
-            print("incorrect guess")
-            guess_counter += 1
+        if guess in char:
+            print(f"{guess} is in the mystery word.")
+
+        else:
+            print("Your guess was incorrect")
+
 
 
     if guess == hidden_word:
@@ -27,12 +29,12 @@ while guess_counter < 6:
         break
 
     elif guess != hidden_word:
-        guess_counter += 1
-        print(f"That letter does NOT appear in the word try again. {guess_counter} out of 6 guesses.")
+        guess_counter -= 1
+        print(f"That letter does NOT appear in the word try again. You have {guess_counter} guesses.")
 
 
 
     else:
         break
-if guess_counter == 6:
+if guess_counter == 0:
     print("You have run out of guesses, you lose!")
