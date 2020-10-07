684a6dbe3040fd748246ba8154b943ff18be1169
MichaelHagler <juggleforlife@gmail.com>
2020-10-07 06:45:02 -0700

    Update hangman.py

--- hangman.py
+++ hangman.py
@@ -2,6 +2,8 @@
 import random
 
 words = ["pie", "programmer", "python"]
+correct_guesses = []
+wrong_guesses = []
 hidden_word = random.choice(list(words))
 guess_counter = 6
 
@@ -15,11 +17,15 @@ while guess_counter > 0:
 
     # check if letter is in hidden_word
     for char in hidden_word:
-        if guess in char:
-            print(f"{guess} is in the mystery word.")
+        if char == guess:
+            correct_guesses.append(guess)
+            print(f"{guess} is right! This is what you've guessed right so far:", *correct_guesses)
 
-        else:
-            print("Your guess was incorrect")
+        elif guess != char:
+            wrong_guesses.append(guess)
+            print(f"{guess} was wrong. This is what you've guessed wrong so far:", *wrong_guesses)
+            guess_counter -= 1
+            print(f"You have {guess_counter} guesses left.")
 
 
 
@@ -28,13 +34,6 @@ while guess_counter > 0:
         print("You guessed the word, you win!")
         break
 
-    elif guess != hidden_word:
-        guess_counter -= 1
-        print(f"That letter does NOT appear in the word try again. You have {guess_counter} guesses.")
 
-
-
-    else:
-        break
 if guess_counter == 0:
     print("You have run out of guesses, you lose!")
